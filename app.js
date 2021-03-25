06bcb77116f43f554a104393e7ca110815d5207f
konstantin zhilyaev <k926227@gmail.com>
2021-03-25 22:29:17 +0300

    refactor: rule 3.4 add some new classes and apply rule - don't use class or id selector with tag selector

--- app.js
+++ app.js
@@ -33,18 +33,19 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className='task';
+  label.className='task-label';
 
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task-name";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
   editButton.className="edit";
 
   deleteButton.className="delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.className="delete-button-img";
   deleteButton.appendChild(deleteButtonImg);
 
 
